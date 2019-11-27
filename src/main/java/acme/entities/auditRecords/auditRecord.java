54bb8cf6c96b5cb2425aa8151454f7beafd2ab3c
Rolan <joscorfis@alum.us.es>
2019-11-27 13:06:39 +0100

    Task 020 & Task 021: Auditor and AuditRecord finished.

---
+++ src/main/java/acme/entities/auditRecords/auditRecord.java
@@ -0,0 +1,61 @@
+
+package acme.entities.auditRecords;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import acme.entities.jobs.Job;
+import acme.entities.jobs.Status;
+import acme.entities.roles.Auditor;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class auditRecord extends DomainEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	private String				title;
+
+	@NotNull
+	private Status				status;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				moment;
+
+	@NotBlank
+	private String				body;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Job					job;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Auditor				auditor;
+
+}
