4ba8d93cc603cc1e840b2507a7dbc212718951e5
rebla <rebla@10.100.24.219>
2019-12-03 18:06:07 +0100

    Auditor Records added in authenticated

---
+++ src/main/java/acme/features/authenticated/auditRecord/AuthenticatedAuditRecordRepository.java
@@ -0,0 +1,30 @@
+
+package acme.features.authenticated.auditRecord;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.auditRecords.AuditRecord;
+import acme.entities.jobs.Job;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AuthenticatedAuditRecordRepository extends AbstractRepository {
+
+	@Query("select a from AuditRecord a where a.id=?1")
+	AuditRecord findOneAuditRecordById(int id);
+
+	@Query("select a from AuditRecord a where (a.job.id=?1 and a.status=1)")
+	Collection<AuditRecord> findManyAuditRecordByJobId(int idJob);
+
+	@Query("select count(a) from AuditRecord a where a.id=?1 and (a.status=1 or a.auditor.id=?2)")
+	int findPublishedOrOwnAuditRecord(int idAuditRecord, int idAuditor);
+
+	@Query("select j from Job j where j.id=?1")
+	Job findJob(int jobId);
+
+	@Query("select a.job from AuditRecord a where a.id=?1")
+	Job findJobByAuditRecordId(int auditRecordId);
+}
